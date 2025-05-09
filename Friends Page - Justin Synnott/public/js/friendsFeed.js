848c77870bd8eb689211e820496793694a8fa388
snottypeen <119279210+snottypeen@users.noreply.github.com>
2025-05-09 15:46:39 -0400

    Update friendsFeed.js

---
+++ Friends Page - Justin Synnott/public/js/friendsFeed.js
@@ -0,0 +1,82 @@
+//Adding Posts
+const postForm = document.getElementById("post-form");
+const postText = document.getElementById("post-text");
+const postFeed = document.querySelector(".posts-feed");
+
+postForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const text = postText.value.trim();
+    if(!text || text.length === 0) return alert("Cannot add an empty post!");
+
+    const requestConfig = {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({body: text})
+    };
+    fetch("/friends/post", requestConfig).then(async (response) => {
+        if(!response.ok) return alert("Failed to add post");
+
+        const newPost = await response.json();
+
+        const postDiv = document.createElement('div');
+        postDiv.className = "post-item";
+        postDiv.dataset.postId = newPost._id;
+        postDiv.innerHTML = 
+            `<h4 class="friend-poster">${newPost.name} &emsp; ${newPost.postedDate}</h4>
+            <p class="friend-post">${newPost.body}</p>
+
+            <button class="like-button">Like</button>
+            <p class="post-likes">Current Likes: 0</p>
+
+            <div class="post-comments"></div>
+
+            <form method="POST" class="comment-form">
+                <input type="text" class="comment-input" placeholder="Write a comment">
+                <button type="submit" class="add-comment">Comment</button>
+            </form>`;
+        postFeed.prepend(postDiv);
+        postText.value = "";
+
+        const commentForm = postDiv.querySelector(".comment-form");
+        commentForm.addEventListener("submit", addComment);
+
+    });
+});
+
+//Adding Comments
+function addComment(event){
+    event.preventDefault();
+
+    const commentForm = event.target;
+    const commentInput = commentForm.querySelector(".comment-input");
+    const postId = commentForm.closest(".post-item").dataset.postId;
+
+    const comment = commentInput.value.trim();
+    if(!comment || comment.length === 0) return alert("Comment cannot be empty!");
+
+    const requestConfig = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({postId, comment})
+    };
+    fetch("/friends/comment", requestConfig).then(async (response) => {
+        if(!response.ok) return alert("Failed to add comment");
+
+        const newComment = await response.json();
+
+        const comments = commentForm.previousElementSibling;
+        const commentDiv = document.createElement("div");
+        commentDiv.innerHTML = 
+            `<p class="comment-poster">${newComment.name} &emsp; ${newComment.postedDate}</p>
+            <p class="comment-body">${newComment.comment}</p>`;
+        
+        comments.appendChild(commentDiv);
+        commentInput.value = "";
+
+    });
+};
+
+document.querySelectorAll(".comment-form").forEach(form => {
+    form.addEventListener("submit", addComment);
+});
